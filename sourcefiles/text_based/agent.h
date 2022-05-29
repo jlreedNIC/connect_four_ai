bde0ebf98766c7767276545ef13d40165cc6c1e8
Jordan Reed <jlreed@students.nic.edu>
2022-05-29 00:26:21 -0700

    Agent class file has bare bones agent functions. Can play against AI now.

--- sourcefiles/text_based/agent.h
+++ sourcefiles/text_based/agent.h
@@ -2,6 +2,7 @@
 #define AGENT_H
 
 #include <iostream>
+#include <algorithm>
 
 class AgentConnect4
 {
@@ -9,7 +10,26 @@ class AgentConnect4
         AgentConnect4();
         ~AgentConnect4();
 
+        int pickMove(int arrBoard[][7]);
+
     private:
+        int maxValue(int state[][7], int move, int depth);
+        int minValue(int state[][7], int move, int depth);
+
+        // checking for a win
+        // functions copied from textApp.h
+        // find something more efficient
+        int checkForWin(int arrBoard[][7]);
+        int horizWinCheck(int arrBoard[][7], int i, int j, int winstate);
+        int vertWinCheck(int arrBoard[][7], int i, int j, int winstate);
+        int diagWinCheck(int arrBoard[][7], int i, int j, int winstate);
+
+        void placeToken(int arrBoard[][7], int player, int position);
+
+        bool checkForDraw(int arrBoard[][7]);
+
+
+        int maxDepth;
 
 };
 
